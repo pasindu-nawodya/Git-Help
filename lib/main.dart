cff8c114449d943f6472cbe74059c3103dae8591
Damsiri Dilanjan <52312934+DamsiriDilanjan@users.noreply.github.com>
2021-06-14 18:26:04 +0530

    Login & Signup UI

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:githelp/constant.dart';
 import 'package:githelp/screen/home/home.dart';
+import 'package:githelp/screen/Welcome/welcome_screen.dart';
 
 
 void main() {
@@ -21,30 +22,8 @@ class MyApp extends StatelessWidget {
           textTheme: TextTheme(
             body1: TextStyle(color: kBodyTextColor),
           )),
-      home: MyHomePage(title: 'Git Helper for Beginners'),
+      home: WelcomeScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  void initState() {
-    super.initState();
-    Timer(
-        Duration(seconds: 5),
-            () => Navigator.push(
-            context, MaterialPageRoute(builder: (context) => Home())));
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold();
-  }
-}
